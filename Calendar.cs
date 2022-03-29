e8827b04e6cdb9b5a91d712133f21a7d45b4a57f
Uma Bangalore <umab@microsoft.com>
2022-03-29 09:07:05 -0700

    Add project files.

---
+++ Calendar.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlockTime
+{
+    class Calendar
+    {
+        /// <summary>
+        /// Calculate all free time slots on a calendar given a calendar, bounds and meeting duration
+        /// </summary>
+        /// <param name="inputCalendar"></param>
+        /// <param name="bounds"></param>
+        /// <param name="meetingDuration"></param>
+        /// <returns>List of available time slots</returns>
+        public List<Tuple<string, string>> AllFreeTimes(string inputCalendar, string bounds, int meetingDuration)
+        {
+            string[] freeTimePeriods = ReFormatForFreeTimeSlots(inputCalendar, bounds);
+            List<Tuple<string, string>> freeTimeSlots = new List<Tuple<string, string>>();
+            TimeSpan interval = new TimeSpan(0, meetingDuration, 00);
+
+            foreach (string period in freeTimePeriods)
+            {
+                Tuple<string, string> tPeriod = FormatTimeSlotToTuple(period);
+                if (!string.Equals(tPeriod.Item1, tPeriod.Item2))
+                {
+                    string lowerBound = tPeriod.Item1;
+                    string upperBound = tPeriod.Item2;
+
+                    DateTime timeSlotLowerBound = Format24HrTime(lowerBound, 2022, 2, 2);
+                    DateTime timeSlotUpperBound = Format24HrTime(upperBound, 2022, 2, 2);
+
+                    DateTime timeSlotNextBound = timeSlotLowerBound.Add(interval);
+
+                    while (timeSlotNextBound <= timeSlotUpperBound)
+                    {
+                        freeTimeSlots.Add(Tuple.Create(timeSlotLowerBound.ToString("HH:mm"), timeSlotNextBound.ToString(("HH:mm"))));
+                        timeSlotLowerBound = timeSlotNextBound;
+                        timeSlotNextBound = timeSlotLowerBound.Add(interval);
+                    }                   
+                }
+            }
+            return freeTimeSlots;
+        }
+
+        /// <summary>
+        /// Print all time slots in the specified format
+        /// </summary>
+        /// <param name="timeslots"></param>
+        public void PrintTimeSlots(List<Tuple<string, string>> timeslots)
+        {
+            string toPrint = "[";
+
+            foreach (Tuple<string, string> slot in timeslots)
+            {
+                toPrint += string.Format("['{0}', '{1}'], ", slot.Item1, slot.Item2);
+            }
+
+            toPrint = toPrint.Remove(toPrint.Length - 2, 2);  // remove the last ", " character
+            toPrint += "]";
+
+            Console.WriteLine(toPrint);
+        }
+
+        /// <summary>
+        /// Format the input calendar string by extracting the free slots, and also adding the lower and upper bounds
+        /// while creating the free time slots
+        /// </summary>
+        /// <param name="inputCalendar"></param>
+        /// <param name="bounds"></param>
+        /// <returns>A string array of all free time slots in the calendar</returns>
+        private string[] ReFormatForFreeTimeSlots(string inputCalendar, string bounds)
+        {
+            string dropLeadingTrailingBracket = inputCalendar.Trim().Substring(1, inputCalendar.Length - 2);
+
+            string[] freeTimeSlots = dropLeadingTrailingBracket.Split("',", StringSplitOptions.None);
+
+            // Include the lower and upper time bounds
+            // Its possible we end up with ['9:00', '9:00'] but that will be dropped later
+            freeTimeSlots[0] = bounds.Split(",", StringSplitOptions.None)[0] + "'," + freeTimeSlots[0];
+            freeTimeSlots[^1] = freeTimeSlots[^1] + "'," + bounds.Split(",", StringSplitOptions.None)[1];
+
+            return freeTimeSlots;
+        }
+
+        /// <summary>
+        /// Helper function to convert to datetime
+        /// This is used to calculate time slots based on meeting duration
+        /// Note that the year, day, month are arbitrary here and not really used for the calculations
+        /// We only care about the hour and minute for our calculations
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        /// <returns>Datetime representing a bound on the time slot</returns>
+        private static DateTime Format24HrTime(string time, int year, int month, int day)
+        {
+            // hour 
+            string[] hourMin = time.Split(":");
+            int hourInt = int.Parse(hourMin[0]);
+            if (hourInt >= 24)
+            {
+                throw new ArgumentOutOfRangeException("Invalid hour");
+            }
+            // minute
+            int minuteInt = int.Parse(hourMin[1]);
+            if (minuteInt >= 60)
+            {
+                throw new ArgumentOutOfRangeException("Invalid minute");
+            }
+            // year, day, month is arbitrary
+            return new DateTime(year, month, day, hourInt, minuteInt, 0);
+        }
+
+        /// <summary>
+        /// Helper function to format timeslot string to Tuple
+        /// </summary>
+        /// <param name="inputTimeSlot"></param>
+        /// <returns></returns>
+        private Tuple<string, string> FormatTimeSlotToTuple(string inputTimeSlot)
+        {
+            string[] charsToRemove = new string[] { "[", "]", "'"};
+            foreach (var c in charsToRemove)
+            {
+                inputTimeSlot = inputTimeSlot.Trim().Replace(c, string.Empty);
+            }
+
+            string[] times = inputTimeSlot.Split(",", StringSplitOptions.None);
+            Tuple<string, string> timeslot = Tuple.Create(times[0], times[1]);
+
+            return timeslot;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlockTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calendar calendar = new Calendar();
+
+            IntroText();
+
+            int mode = Convert.ToInt32(Console.ReadLine());
+
+            string bounds1, bounds2, c1, c2 = string.Empty;
+            int meetingDuration = 0;
+
+            if (mode == 1)
+            {
+                bounds1 = "['9:00', '20:00']";
+                bounds2 = "['10:00', '18:30']";
+
+                c1 = "[['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']]";
+                c2 = "[[10:00', '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']";
+
+                meetingDuration = 30;
+            }
+            else
+            {
+                Console.Write("Enter first calendar times: ");
+                c1 = Console.ReadLine();
+                Console.Write("Enter the first calendar lower and upper bounds: ");
+                bounds1 = Console.ReadLine();
+
+                Console.Write("Enter second calendar times: ");
+                c2 = Console.ReadLine();
+                Console.Write("Enter the second calendar lower and upper bounds: ");
+                bounds2 = Console.ReadLine();
+
+                Console.Write("How long is the meeting for: ");
+                meetingDuration = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine("All free times are as listed:"); 
+            }
+
+            List<Tuple<string, string>> freeTimes1 = calendar.AllFreeTimes(c1, bounds1, meetingDuration);
+            List<Tuple<string, string>> freeTimes2 = calendar.AllFreeTimes(c2, bounds2, meetingDuration);
+
+            var commonFreeTimes = freeTimes1.Where(t => freeTimes2.Contains(t));
+
+            List<Tuple<string, string>> result = commonFreeTimes.ToList();
+
+            Console.WriteLine();
+            if (result.Count != 0)
+            {
+                Console.WriteLine("All common free time slots are as listed:");
+                calendar.PrintTimeSlots(result);
+            }
+            else
+            {
+                Console.WriteLine("No common free time slots available");
+            }
+        }
+
+        static void IntroText()
+        {
+            Console.WriteLine("Choose mode by typing 1 or 2:");
+            Console.WriteLine("1. Demo mode with preset inputs");
+            Console.WriteLine("2. User mode where you supply the inputs");
+            Console.WriteLine();
+        }
+    }
+}
+
+
